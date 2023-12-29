fc3f161668b438131e8c115479d9d1f0534fd8b0
Gustavo <guortiz098@hotmail.com>
2023-12-29 23:15:31 +0100

    fixed LiveCasino error "lives" typo

--- src/components/LiveCasino.vue
+++ src/components/LiveCasino.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div class="flexCenter my-20 flex-col">
     <div class="flex justify-between text-white xl:w-[60%] md:w-[70%] w-[90%] mb-5">
@@ -8,9 +5,9 @@
       <span>Show All ></span>
     </div>
     <div class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 xl:w-[60%] md:w-[70%] w-[90%]">
-      <div v-for=" live in lives" :key="live.id" class="flex flex-col">
-        <img :src="live.image || 'https://placehold.co/300x200'" alt=" img">
-        <p class="text-white bg-preto py-2 flexCenter">{{ live.title }}</p>
+      <div v-for="item in lives" :key="item.id" class="flex flex-col">
+        <img :src="item.image || 'https://placehold.co/300x200'" alt=" img">
+        <p class="text-white bg-preto py-2 flexCenter">{{ item.title }}</p>
       </div>
     </div>
   </div>
@@ -19,11 +16,10 @@
 <script setup lang="ts">
 
 
-
 interface Live {
-    id: number;
-    title: string;
-    image?: string;
+  id: number;
+  title: string;
+  image: string;
 }
 
 const lives: Live[] = [
@@ -34,7 +30,7 @@ const lives: Live[] = [
   { id: 5, title: 'AUTO ROULETTE', image: '' },
   { id: 6, title: 'VEGAS WORLD', image: '' },
   { id: 7, title: 'POWER BLACKJACK', image: '' },
-  { id: 8, title: 'BLACKJACJ A', image: '' },
+  { id: 8, title: 'BLACKJACK A', image: '' },
   { id: 9, title: 'MUSIC WHEEL', image: '' },
   { id: 10, title: 'BACCARAT A', image: '' },
   { id: 11, title: 'RED DOOR ROULETTE', image: '' },
