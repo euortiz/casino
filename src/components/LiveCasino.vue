1ce8883e46d440d040bfad0ac832308b3e29bb74
Gustavo Ortiz <guortiz098@hotmail.com>
2023-12-29 16:32:19 +0100

    live casino section implemented

--- src/components/LiveCasino.vue
+++ src/components/LiveCasino.vue
@@ -2,7 +2,42 @@
 </script>
 
 <template>
-  <div>
-
+  <div class="flexCenter my-20 flex-col">
+    <div class="flex justify-between text-white w-[60%] mb-5">
+      <span><strong>LIVE CASINO</strong></span>
+      <span>Show All ></span>
+    </div>
+    <div class="grid grid-cols-4 gap-2 w-[60%]">
+      <div v-for=" live in lives" :key="live.id">
+        <img :src="live.image || 'https://placehold.co/300x200'" alt=" img">
+        <p class="text-white bg-preto py-2 flexCenter">{{ live.title }}</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+
+
+
+interface Live {
+    id: number;
+    title: string;
+    image?: string;
+}
+
+const lives: Live[] = [
+  { id: 1, title: 'ROULETTE', image: '' },
+  { id: 2, title: 'ROULETTE GOLD 2', image: '' },
+  { id: 3, title: 'ROULETTE GOLD 3', image: '' },
+  { id: 4, title: 'EBET', image: '' },
+  { id: 5, title: 'AUTO ROULETTE', image: '' },
+  { id: 6, title: 'VEGAS WORLD', image: '' },
+  { id: 7, title: 'POWER BLACKJACK', image: '' },
+  { id: 8, title: 'BLACKJACJ A', image: '' },
+  { id: 9, title: 'MUSIC WHEEL', image: '' },
+  { id: 10, title: 'BACCARAT A', image: '' },
+  { id: 11, title: 'RED DOOR ROULETTE', image: '' },
+  { id: 12, title: 'LIGHTNING BACCARAT', image: '' },
+];
+</script>
