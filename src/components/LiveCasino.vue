9693315bd8d2ff812a732c44dc811f6c4792bb91
Gustavo Ortiz <guortiz098@hotmail.com>
2024-01-02 09:31:24 +0100

    livecasino section implemented

--- src/components/LiveCasino.vue
+++ src/components/LiveCasino.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flexCenter my-20 flex-col">
     <div class="flex justify-between text-white xl:w-[60%] md:w-[70%] w-[90%] mb-5">
-      <span><strong>LIVE CASINO</strong></span>
+      <span><strong>📺 LIVE CASINO</strong></span>
       <span>Show All ></span>
     </div>
     <div class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 xl:w-[60%] md:w-[70%] w-[90%]">
-      <div v-for="item in lives" :key="item.id" class="flex flex-col">
-        <img :src="item.image || 'https://placehold.co/300x200'" alt=" img">
+      <div v-for="item in lives" :key="item.id" class="bg-preto/70 rounded-xl  border-4 border-preto">
+        <img :src="item.image || 'https://placehold.co/300x200'" alt="img" class="w-full h-40 object-cover rounded-t-xl ">
         <p class="text-white bg-preto py-2 flexCenter">{{ item.title }}</p>
       </div>
     </div>
@@ -14,6 +14,18 @@
 </template>
 
 <script setup lang="ts">
+import auto from '../assets/livecasino/auto.svg'
+import baccarat from '../assets/livecasino/baccarat.svg'
+import bjack from '../assets/livecasino/bjack.svg'
+import ebet from '../assets/livecasino/ebet.svg'
+import goldtwo from '../assets/livecasino/goldtwo.svg'
+import goldthree from '../assets/livecasino/goldthree.svg'
+import light from '../assets/livecasino/light.svg'
+import musicw from '../assets/livecasino/musicw.svg'
+import power from '../assets/livecasino/power.svg'
+import reddoor from '../assets/livecasino/reddoor.svg'
+import vegasworld from '../assets/livecasino/vegasworld.svg'
+import roulette from '../assets/livecasino/roulette.svg'
 
 
 interface Live {
@@ -23,17 +35,17 @@ interface Live {
 }
 
 const lives: Live[] = [
-  { id: 1, title: 'ROULETTE', image: '' },
-  { id: 2, title: 'ROULETTE GOLD 2', image: '' },
-  { id: 3, title: 'ROULETTE GOLD 3', image: '' },
-  { id: 4, title: 'EBET', image: '' },
-  { id: 5, title: 'AUTO ROULETTE', image: '' },
-  { id: 6, title: 'VEGAS WORLD', image: '' },
-  { id: 7, title: 'POWER BLACKJACK', image: '' },
-  { id: 8, title: 'BLACKJACK A', image: '' },
-  { id: 9, title: 'MUSIC WHEEL', image: '' },
-  { id: 10, title: 'BACCARAT A', image: '' },
-  { id: 11, title: 'RED DOOR ROULETTE', image: '' },
-  { id: 12, title: 'LIGHTNING BACCARAT', image: '' },
+  { id: 1, title: 'ROULETTE', image: roulette },
+  { id: 2, title: 'ROULETTE GOLD 2', image: goldtwo },
+  { id: 3, title: 'ROULETTE GOLD 3', image: goldthree },
+  { id: 4, title: 'EBET', image: ebet },
+  { id: 5, title: 'AUTO ROULETTE', image: auto },
+  { id: 6, title: 'VEGAS WORLD', image: vegasworld },
+  { id: 7, title: 'POWER BLACKJACK', image: power },
+  { id: 8, title: 'BLACKJACK A', image: bjack },
+  { id: 9, title: 'MUSIC WHEEL', image: musicw },
+  { id: 10, title: 'BACCARAT A', image: baccarat },
+  { id: 11, title: 'RED DOOR ROULETTE', image: reddoor },
+  { id: 12, title: 'LIGHTNING BACCARAT', image: light },
 ];
 </script>
