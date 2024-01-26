54f509f16c9c4d7c239d049a865ea5f9294ff443
Gustavo Ortiz <101819528+euortiz@users.noreply.github.com>
2024-01-26 10:34:31 +0100

    jackpots bg image

--- src/components/Jackpots.vue
+++ src/components/Jackpots.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="my-20 max-container padding-container text-nowrap" v-motion-slide-visible-left>
+    <div class="bg my-20  text-nowrap h-[30rem] flex items-center justify-center" v-motion-slide-visible-left>
         <son-jackpot type="total" animate="true" duration="8000" v-pre class="text-white"></son-jackpot>
     </div>
 </template>
@@ -44,7 +44,14 @@ onMounted(() => {
 <style>
 :root {
     --jackpot-font-weight: 700;
-    --jackpot-font-size: 50px;
+    --jackpot-font-size: 80px;
     --jackpot-text-align: center;
 }
+
+/* .bg {
+    background-image: url('../assets/jackpots/casino_gril.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: top;
+} */
 </style>
